0ee13ab00a832c74e090b8d43eaa059ecbcf5110
r(0_o)t <50882504+aerosol-can@users.noreply.github.com>
2022-06-11 07:55:16 +0530

    fixed for sure

--- tekster.py
+++ tekster.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 import os
-import random
-import base64
+
 def clr():
 	if os.name == 'nt':
 		os.system('cls')
 	else:
 		os.system('clear')
-
 clr()
 print("This Tool Is Used To Send Anonyous Messages\n\n")
 print("Make sure you have proxychains setup.\n\n")
@@ -18,4 +16,18 @@ if '+' in cc:
         tc.remove('+')
         cc = ''.join(tc)
         cc = cc.strip()
-if len(cc) 
+if len(cc) >= 4 or len(cc) < 1:
+        print('\n\nInvalid Country Code..\n\t\tCountry Codes Are Generally 1-3 digits...\n')
+        exit()
+pn = input("Enter Phone Number(Without Country Code) : ")
+if len(pn) <= 6:
+        print('\n\nInvalid Phone Number..\n')
+        banner()
+        exit()
+number = cc + pn
+receiver = '+' + number
+text = input("Enter Message to send : ")
+os.system('''proxychains curl --request POST https://textbelt.com/text \
+    --data-urlencode phone='{}' \
+    --data-urlencode message='{}' \
+    -d key=textbelt'''.format(number,text))
